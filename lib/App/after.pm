27c5dc0be482f9e544f7ec6f4a8b749096dab2c8
perlancar (@pc-home) <perlancar@gmail.com>
2016-04-17 11:12:45 +0700

    Release v0.01

--- lib/App/after.pm
+++ lib/App/after.pm
@@ -13,15 +13,18 @@ our %SPEC;
 #our @ISA       = qw(Exporter);
 #our @EXPORT_OK = qw(after);
 
+our $DEBUG = $ENV{DEBUG};
+
 $SPEC{after} = {
     v => 1.1,
     summary => 'Run command after delay and/or some other event',
     description => <<'_',
 
 `after` will run command after all the specified conditions are met. A condition
-can be: delay (`--delay`), time (`--time`), system load falling below a certain
-value (`--load-below`), system load rising above a certain value
-(`--load-above`). More conditions will be available in the future.
+can be: delay (`--delay`), current time (`--time`), system uptime (`--uptime`),
+system load falling below a certain value (`--load-below`), system load rising
+above a certain value (`--load-above`). More conditions will be available in the
+future.
 
 _
     args => {
@@ -29,6 +32,7 @@ _
             schema => ['array*', of=>'str*', min_len=>1],
             req => 1,
             pos => 0,
+            greedy => 1,
         },
 
         delay => {
@@ -42,11 +46,18 @@ _
             'x.perl.coerce_to' => 'int(epoch)',
             tags => ['category:condition'],
         },
+        uptime => {
+            schema => 'duration*',
+            'x.perl.coerce_to' => 'int(secs)',
+            tags => ['category:condition'],
+        },
         load_below => {
-            schema => ['int*', min=>0],
+            schema => ['float*', min=>0],
+            tags => ['category:condition'],
         },
         load_above => {
-            schema => ['int*', min=>0],
+            schema => ['float*', min=>0],
+            tags => ['category:condition'],
         },
         # XXX: condition: we are online
         # XXX: condition: we are offline
@@ -59,11 +70,13 @@ _
             summary =>
                 'Run command after one condition (instead of all) is met',
             schema => 'bool',
+            tags => ['category:logic'],
         },
         none => {
             summary =>
                 'Run command when none of the conditions are met',
             schema => 'bool',
+            tags => ['category:logic'],
         },
     },
     args_rels => {
@@ -81,9 +94,39 @@ _
             test => 0,
             'x.doc.show_result' => 0,
         },
+        {
+            argv => ['--delay', '12h', '--time', '2016-04-18', 'cmd'],
+            summary => 'Run command after 12 hour delay and time has passed 2016-04-18',
+            test => 0,
+            'x.doc.show_result' => 0,
+        },
+        {
+            argv => ['--or', '--delay', '12h', '--time', '2016-04-18', 'cmd'],
+            summary => 'Run command after 12 hour delay *or* time has passed 2016-04-18',
+            test => 0,
+            'x.doc.show_result' => 0,
+        },
+        {
+            argv => ['--uptime', '2d', 'cmd'],
+            summary => 'Run command after system uptime is 2 days',
+            test => 0,
+            'x.doc.show_result' => 0,
+        },
+        {
+            argv => ['--load-above', '2.5', 'cmd'],
+            summary => 'Run command after system load is above 2.5',
+            test => 0,
+            'x.doc.show_result' => 0,
+        },
+        {
+            argv => ['--load-above', '1', '--load-below', 5, 'cmd'],
+            summary => 'Run command after system load is between 1 and 5',
+            test => 0,
+            'x.doc.show_result' => 0,
+        },
     ],
     #result_naked => 1,
-}
+};
 sub after {
     my %args = @_;
     my $cmd = $args{command};
@@ -95,13 +138,32 @@ sub after {
     if (defined $args{delay}) {
         push @conds, {
             freq => 1,
-            check => sub { time() >= $start_time() + $args{delay} },
+            check => sub {
+                my $now = time();
+                say "D:Checking delay: current=", ($now-$start_time) if $DEBUG;
+                $now >= $start_time + $args{delay};
+            },
         };
     }
     if (defined $args{time}) {
         push @conds, {
             freq => 1,
-            check => sub { time() >= $args{time} },
+            check => sub {
+                my $now = time();
+                say "D:Checking time: current=$now, target=$args{time}" if $DEBUG;
+                $now >= $args{time};
+            },
+        };
+    }
+    if (defined $args{uptime}) {
+        require Unix::Uptime;
+        push @conds, {
+            freq => 1,
+            check => sub {
+                my $uptime = Unix::Uptime->uptime;
+                say "D:Checking uptime: current=$uptime, target=$args{uptime}" if $DEBUG;
+                $uptime >= $args{uptime};
+            },
         };
     }
     if (defined($args{load_below}) || defined($args{load_above})) {
@@ -110,6 +172,7 @@ sub after {
             freq => 5,
             check => sub {
                 my @load = Unix::Uptime->load();
+                say "D:Checking load_below/load_above: current=$load[0]" if $DEBUG;
                 return 0 if defined($args{load_below}) &&
                     $load[0] >= $args{load_below};
                 return 0 if defined($args{load_above}) &&
@@ -119,15 +182,19 @@ sub after {
         };
     }
 
+    goto RUN_PROGRAM unless @conds;
+
+    $_->{counter} = 0 for @conds;
+
     while (1) {
         my $num_checked = 0;
         my $num_met = 0;
         for my $cond (@conds) {
-            $cond->{counter}++;
-            last unless $cond->{counter} >= $cond->{freq};
+            $cond->{counter}--;
+            next unless $cond->{counter} <= 0;
             $num_checked++;
-            $cond->{counter} = 0;
-            last unless $cond->{check}->();
+            $cond->{counter} = $cond->{freq};
+            next unless $cond->{check}->();
             $num_met++;
             last if $args{or} || $args{none};
         }
@@ -145,15 +212,23 @@ sub after {
         sleep 1;
     }
 
+  RUN_PROGRAM:
     system {$cmd->[0]} @$cmd;
-    my $exit_code = $?;
+    my $num = $?;
 
-    return [
-        $exit_code ? 500 : 200,
-        $exit_code ? "Program failed" : "OK",
-        undef,
-        {"cmdline.exit_code" => $exit_code},
-    ];
+    if ($num == -1) {
+        return [500, "Program failed to run: $!"];
+    } elsif ($num) {
+        my $exit_code = $num >> 8;
+        return [
+            500,
+            "Program exit non-success (exit code $exit_code)",
+            undef,
+            {"cmdline.exit_code" => $exit_code},
+        ];
+    } else {
+        return [200, "OK"];
+    }
 }
 
 1;
@@ -162,3 +237,8 @@ sub after {
 =head1 DESCRIPTION
 
 See the included script L<after>.
+
+
+=head1 ENVIRONMENT
+
+=head2 DEBUG
